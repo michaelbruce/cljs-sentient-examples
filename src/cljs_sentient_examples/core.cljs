684b0e1f616899a9ecd9f041a795beb1df265280
Michael Bruce <michael.bruce@singletracksystems.com>
2016-05-07 01:22:34 +0100

    Interface complete

--- src/cljs_sentient_examples/core.cljs
+++ src/cljs_sentient_examples/core.cljs
@@ -6,15 +6,26 @@
 (println "Edits to this text should show up in your developer console.")
 
 ;; define your app data so that it doesn't get over-written on reload
+;; TODO write a cljs library like honeysql for using sentient?
+;; TODO if y doesn't need to be global, push it down as a let inside defn input
+;; TODO include randomise button to insert all values.
 
 (defonce app-state (atom {:text "Sentient Test"}))
 
-(defn hello-world []
+(defn input [name]
+  (let [value (atom name)]
+    [:input {:type "text"
+             :placeholder name
+             :class "style-1"
+             :size 2
+             :on-change #(reset! @value (-> % .-target .-value))}]))
+
+(defn interface []
   [:div
    [:h1 (:text @app-state)]
-   [:button "Push Me"]])
+   [:p (input "x") "^3 + " (input "y") "^2 + " (input "x") " - " (input "y") " = " (input "z")]])
 
-(reagent/render-component [hello-world]
+(reagent/render-component [interface]
                           (. js/document (getElementById "app")))
 
 
