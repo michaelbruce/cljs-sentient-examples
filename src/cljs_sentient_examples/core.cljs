b04c41d7f9bfaa1bb782ff2818c0fd0f95a8822f
Michael Bruce <michael.bruce@singletracksystems.com>
2016-05-07 12:14:57 +0100

    on-change works and affected components rerender!

--- src/cljs_sentient_examples/core.cljs
+++ src/cljs_sentient_examples/core.cljs
@@ -3,31 +3,37 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
-
 ;; define your app data so that it doesn't get over-written on reload
 ;; TODO write a cljs library like honeysql for using sentient?
 ;; TODO if y doesn't need to be global, push it down as a let inside defn input
 ;; TODO include randomise button to insert all values.
 
-(defonce app-state (atom {:text "Sentient Test"}))
+(defonce app-state (atom {:text "Sentient Test"
+                          :x ""
+                          :y 4
+                          :z ""}))
 
-(defn input [name]
-  (let [value (atom name)]
+; (defn sentient x + y)
+
+;(defn update-value [value]
+;  (swap! app-state update-in [:x] value))
+
+(defn input [value]
     [:input {:type "text"
-             :placeholder name
+             :placeholder (name value)
+             :value (value @app-state)
              :class "style-1"
              :size 2
-             :on-change #(reset! @value (-> % .-target .-value))}]))
+             :on-change #(swap! app-state assoc value (-> % .-target .-value))}])
 
 (defn interface []
   [:div
    [:h1 (:text @app-state)]
-   [:p (input "x") "^3 + " (input "y") "^2 + " (input "x") " - " (input "y") " = " (input "z")]])
+   [:p "The value of Y is " (:y @app-state)]
+   [:p (input :x) "^3 + " (input :y) "^2 + " (input :x) " - " (input :y) " = " (input :z)]])
 
 (reagent/render-component [interface]
                           (. js/document (getElementById "app")))
-
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
